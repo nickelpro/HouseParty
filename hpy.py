f7d37e321b252987fc762c3932775d5cf92ecb38
Vito Gamberini <vito@gamberini.email>
2023-07-29 02:57:51 -0400

    feat: Turn off intermediate wires

--- hpy.py
+++ hpy.py
@@ -3,11 +3,15 @@
 import sys
 import houseparty
 
-hpmod = houseparty.readFile(sys.argv[1] if len(sys.argv) > 1 else 'test.hpy')
-ir = houseparty.genIR(hpmod)
+use_int = '--no-int' not in sys.argv
+hpmod = houseparty.readFile(sys.argv[1 if use_int else 2])
+ir = houseparty.genIR(hpmod, use_int)
 
-if len(sys.argv) > 2:
+if (use_int and len(sys.argv) > 2):
   with open(sys.argv[2], 'w') as f:
+    f.write(str(ir))
+elif (not use_int and len(sys.argv) > 3):
+  with open(sys.argv[3], 'w') as f:
     f.write(str(ir))
 else:
   print(ir)
